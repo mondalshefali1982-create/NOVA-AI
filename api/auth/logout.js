936695669c0123af098665cee0e792b256386ae9
mondalshefali1982-create <mondalshefali1982@gmail.com>
2026-06-15 22:18:10 +0530

    Delete api directory

--- api/auth/logout.js
+++
@@ -1,9 +0,0 @@
-const { setCors, handleOptions, requirePost } = require("../_lib/gemini");
-
-module.exports = async function handler(req, res) {
-  setCors(res);
-  if (handleOptions(req, res)) return;
-  if (!requirePost(req, res)) return;
-
-  res.status(200).json({ message: "Logged out successfully." });
-};
