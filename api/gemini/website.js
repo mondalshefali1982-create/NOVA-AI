b2095db3d62be013e56733a59882f094409ae9e8
mondalshefali1982-create <mondalshefali1982@gmail.com>
2026-06-23 13:15:40 +0530

    Update website.js

--- api/gemini/website.js
+++ api/gemini/website.js
@@ -1,6 +1,6 @@
 const { callGemini, getBody, handleOptions, requirePost, sendError, setCors } = require("../_lib/gemini");
 
-const WEBSITE_TIMEOUT_MS = 120000;
+const WEBSITE_TIMEOUT_MS = 180000;
 const WEBSITE_RETRIES = 2;
 const REQUIRED_PAGES = ["index", "about", "services", "contact"];
 
