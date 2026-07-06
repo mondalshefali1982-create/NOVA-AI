a661edd0d0104d2a015856b0f27dd09dca3745bd
mondalshefali1982-create <mondalshefali1982@gmail.com>
2026-07-07 00:39:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // If the frontend is deployed on the same Vercel project as /api, leave this empty..
 // If the frontend is on GitHub Pages and the backend is on Vercel, set this to your Vercel URL.
 // Example: window.NOVA_BACKEND_BASE_URL = "https://nova-ai-yourname.vercel.app";
-window.NOVA_BACKEND_BASE_URL = "";
+window.NOVA_BACKEND_BASE_URL = "https://nova-ai-rohann.vercel.app";
